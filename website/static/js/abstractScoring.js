158b9e4a4864dabc3631c3756cab04fcb9e94b89
Lucas Lefebvre <142241432+lucaslef314@users.noreply.github.com>
2025-12-09 19:46:21 -0500

    Fix file name change

--- website/static/js/abstractScoring.js
+++ website/static/js/abstractScoring.js
@@ -53,7 +53,7 @@ if (saveBtn) {
       if (!res.ok) throw new Error('Failed to save grade');
 
       // Redirect back to Abstract Grader Dashboard
-      window.location.href = '/abstractGrader';
+      window.location.href = '/abstract_grader';
     } catch (err) {
       console.error(err);
       alert("Error saving grade: " + err.message);
